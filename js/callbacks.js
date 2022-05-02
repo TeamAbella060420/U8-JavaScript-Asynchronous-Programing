01ec906182a319472888d3eb7e6c5a141deb3b84
TeamAbella060420 <jabella@rscm.com.sa>
2022-05-02 14:31:28 +0300

    Update callbacks.js

--- js/callbacks.js
+++ js/callbacks.js
@@ -4,16 +4,22 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Make an AJAX request
-function getJSON(url) {
+function getJSON(url, callback) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = () => {
     if(xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      return callback(data);
     }
   };
   xhr.send();
+}
+
+function getProfiles(json) {
+  json.people.map( person => {
+    getJSON(wikiUrl + person.name, generateHTML);
+  });
 }
 
 // Generate the markup for each profile
@@ -38,4 +44,7 @@ function generateHTML(data) {
   }
 }
 
-btn.addEventListener('click', () => getJSON(astrosUrl));
+btn.addEventListener('click', (event) => { 
+  getJSON(astrosUrl, getProfiles);
+  event.target.remove();
+});
